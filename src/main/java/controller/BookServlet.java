1d85874fa1646f23129619b5e7749b0478841c1e
angelina <aurumsl@mail.ru>
2026-03-28 21:01:39 +0300

    ЛР2. добавлены сервлеты книги и авторы

---
+++ src/main/java/controller/BookServlet.java
@@ -0,0 +1,36 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloBookServlet")
+public class BookServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public BookServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+
+        try {
+            writer.println("<h2>Привет BookServlet</h2>");
+        } finally {
+            writer.close();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
